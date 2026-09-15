e8b840470fd4a02f42e7c973b303534ee4f04a26
Mateo <mateoresa@gmailo.com>
2026-09-15 15:26:31 -0300

    Clase 23: Conexion de frontend y backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json())
+app.use(cors());
 const alumnosRoutes = require("./routes/alumnos.routes");
 //const docentesRoutes = require("./routes/docentes.routes");
 app.use("/alumnos",alumnosRoutes);
@@ -21,7 +23,7 @@ app.use((req,res,next)=>{
 
 
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log(`Servidor funcionando en http://localhost:${PORT}`);
 })
 
